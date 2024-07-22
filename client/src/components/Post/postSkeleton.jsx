52a95eb37ffe7aab511d4374b7e7079e2da4898c
Dimitar Dimitrov <dimitardimitrov0317@gmail.com>
2024-07-22 15:42:32 +0300

    edit, delete and details buttons added

--- client/src/components/Post/postSkeleton.jsx
+++ client/src/components/Post/postSkeleton.jsx
@@ -1,39 +1,36 @@
-import { React, useState } from 'react';
+import { React } from 'react';
 import './postSkeleton.css';
-import { bookmarkSVG, commentSVG, dislikeSVG, likeSVG, optionsSVG } from './postSVG';
+import { bookmarkSVG, commentSVG, dislikeSVG, likeSVG, deleteSVG, editSVG, detailsSVG } from './postSVG';
 
 export default function PostSkeleton({ text, fullName, username, image }) {
-  const [dropdownVisible, setDropdownVisible] = useState(false);
-
-  const toggleDropdown = () => {
-    setDropdownVisible(!dropdownVisible);
-  };
-
   return (
-    <div className='post-skeleton'>
-      <button className='options-button' onClick={toggleDropdown}>
-        {optionsSVG}
-      </button>
-      <div className={`dropdown-menu ${dropdownVisible ? 'visible' : ''}`}>
-        <div className='dropdown-item'>Details</div>
-        <div className='dropdown-item'>Edit</div>
-        <div className='dropdown-item'>Delete</div>
-      </div>
-      <div className='post-header'>
-        <div className='avatar-skeleton'></div>
-        <div className='header-text'>
-          <div className='name-skeleton'>{fullName}</div>
-          <div className='username-skeleton'>{'@'}{username}</div>
+      <div className='post-skeleton'>
+        <div className="button-container">
+          <button className='options-button'>
+            {editSVG}
+          </button>
+          <button className='options-button'>
+            {deleteSVG}
+          </button>
+          <button className='options-button'>
+            {detailsSVG}
+          </button>
+        </div>
+        <div className='post-header'>
+          <div className='avatar-skeleton'></div>
+          <div className='header-text'>
+            <div className='name-skeleton'>{fullName}</div>
+            <div className='username-skeleton'>{'@'}{username}</div>
+          </div>
+        </div>
+        <div className='content-skeleton'>{text}</div>
+        {image && <div className='image-container'><img src={image} alt="Post" className='post-image' /></div>}
+        <div className='post-functionality'>
+          <button className='functionalities'>{likeSVG}</button>
+          <button className='functionalities'>{dislikeSVG}</button>
+          <button className='functionalities'>{commentSVG}</button>
+          <button className='functionalities'>{bookmarkSVG}</button>
         </div>
       </div>
-      <div className='content-skeleton'>{text}</div>
-      {image && <div className='image-container'><img src={image} alt="Post" className='post-image' /></div>}
-      <div className='post-functionality'>
-        <button className='functionalities'>{likeSVG}</button>
-        <button className='functionalities'>{dislikeSVG}</button>
-        <button className='functionalities'>{commentSVG}</button>
-        <button className='functionalities'>{bookmarkSVG}</button>
-      </div>
-    </div>
   );
 }
