54e0e366ffb74d4022491b8cd42b5a8948d9520c
Dimitar Dimitrov <dimitardimitrov0317@gmail.com>
2024-07-22 19:09:56 +0300

    signUp form connected to signUp API

--- client/src/components/signup/SignUp.jsx
+++ client/src/components/signup/SignUp.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { React, useState } from 'react';
 import { Link } from 'react-router-dom';
 import LogoSvg from "../svg/Logo";
 import { MdOutlineMail, MdDriveFileRenameOutline, MdPassword } from 'react-icons/md';
@@ -6,31 +6,67 @@ import { FaUser } from 'react-icons/fa';
 import './signup.css';
 
 export default function SignUp() {
+  const [formData, setFormData] = useState({
+    email: '',
+    username: '',
+    fullName: '',
+    password: ''
+  });
+
+  function changeHandler(e) {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  async function signUpSubmitHandler(e) {
+    e.preventDefault();
+    console.log(formData);
+    try {
+      const response = await fetch('http://localhost:5000/api/auth/signup', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        credentials: 'include',
+        body: JSON.stringify(formData), //TODO: Refactor this function
+      });
+
+      if (!response.ok) {
+        throw new Error();
+        //TODO Error handling
+      }
+      console.log(response);
+      console.log('Sign up successful!'); //TODO: Remove this
+    } catch (error) {
+      console.error('There was a problem with the signup:', error);
+    }
+  };
+
   return (
     <div className="signup-container">
       <div className="signup-image-container">
-      <LogoSvg className="signup-image" />
+        <LogoSvg className="signup-image" />
       </div>
       <div className="signup-form-container">
-        <form className="signup-form">
+        <form className="signup-form" onSubmit={signUpSubmitHandler}>
           <h1 className="signup-title">Join today.</h1>
           <label className="signup-input-container">
             <MdOutlineMail />
-            <input type="email" className="signup-input" placeholder="Email" name="email" />
+            <input type="email" className="signup-input" placeholder="Email" name="email" onChange={changeHandler} />
           </label>
           <div className="signup-input-group">
             <label className="signup-input-container">
               <FaUser />
-              <input type="text" className="signup-input" placeholder="Username" name="username" />
+              <input type="text" className="signup-input" placeholder="Username" name="username" onChange={changeHandler} />
             </label>
             <label className="signup-input-container">
               <MdDriveFileRenameOutline />
-              <input type="text" className="signup-input" placeholder="Full Name" name="fullName" />
+              <input type="text" className="signup-input" placeholder="Full Name" name="fullName" onChange={changeHandler} />
             </label>
           </div>
           <label className="signup-input-container">
             <MdPassword />
-            <input type="password" className="signup-input" placeholder="Password" name="password" />
+            <input type="password" className="signup-input" placeholder="Password" name="password" onChange={changeHandler} />
           </label>
           <button className="signup-button">Sign up</button>
         </form>
--- server/server.js
+++ server/server.js
@@ -18,7 +18,10 @@ v2.config({
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:5173', 
+    credentials: true
+  }));
 app.use(express.json());
 app.use(urlencoded({ extended: true }));
 app.use(cookieParser());
