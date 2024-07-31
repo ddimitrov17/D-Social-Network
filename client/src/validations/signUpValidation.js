050fcd84d3ee36f8d5d5aad976bd27be89b22d18
Dimitar Dimitrov <dimitardimitrov0317@gmail.com>
2024-07-31 15:57:24 +0300

    Update signUpValidation.js

--- client/src/validations/signUpValidation.js
+++ client/src/validations/signUpValidation.js
@@ -1,8 +1,8 @@
 import * as yup from 'yup';
 
 export const signUpSchema = yup.object().shape({
-    email: yup.string().email('Invalid email format').min(7, 'Email must be at lest 7 characters').required('Email is required'),
-    username: yup.string().min(5, 'Username must be at least 5 characters').required('Username is required'),
-    fullName: yup.string().required('Full Name is required'),
-    password: yup.string().min(4, 'Password must be at least 4 characters').max(12, 'Password cannot exceed 12 characters').required('Password is required'),
+    email: yup.string().email().min(7).required(),
+    username: yup.string().min(5).required(),
+    fullName: yup.string().required(),
+    password: yup.string().min(4).max(12).required(),
 });
